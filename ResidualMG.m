32dd43036217dc5a3c6a26aa67578686742aa383
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-03-20 18:24:30 +0100

    function updated

--- ResidualMG.m
+++ ResidualMG.m
@@ -1,11 +1,15 @@
 ## MG marginal analysis of residuals from LegReg
-## usage:   intv = ResidualMG(para);
-##          data = residuals data
-
+## usage:   intv = ResidualMG(filename, resid);
+##          resid = residuals data
+##          filename = data filename
 
 ## MG marginal analysis
-function intv = ResidualMG(resid);
+function intv = ResidualMG(filename, resid);
 para = dfchoice
 para.data = resid;
 [result, intv, LowerCluster, UpperCluster, ERR] = zw_marginal_analysis_egdf(para);
+# gnp file generation and pdf
+gnp = prepgnplot(result);
+fileoutput = strcat("data/", "gnp-",strrep(filename,'.csv',''), ".text);
+gnsvplot(gnp, fileoutput);
 endfunction
--- analysis.m
+++ analysis.m
@@ -5,7 +5,7 @@
 LegReg_result = LegReg(filename='hexanol_109.csv', order=3);
 
 # MG marginal analysis of residual from LegReg
-intv = ResidualMG(LegReg_result.resid);
+intv = ResidualMG(filename, LegReg_result.resid);
 
 # plotting results and data
 plt = RegPlot(filename, LegReg_result.fitY);
