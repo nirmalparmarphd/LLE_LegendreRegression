125cffbee2242d64f31f6781cb9615a5db1aa067
nirmalparmar_ws <nirmalparmarphd@gmail.com>
2023-03-20 13:42:17 +0100

    code updated

--- DataParser.m
+++ DataParser.m
@@ -3,7 +3,7 @@
 
 function [x_values, y_values] = DataParser(data);
 
-x_values = data(:,2); # X values from the LLE data
+x_values = data(:,2); # X mol fraction values from the LLE data
 y_values = data(:,3); # T (K) values from the LLE data
 
 endfunction
--- LLE_DataAnalysis.m
+++ LLE_DataAnalysis.m
@@ -5,7 +5,11 @@ clear
 pkg load io
 
 ## loading lle data
-lle_data = DataLoading('hexanol_109.csv')
+filename = ('hexanol_109.csv');
+plottitle = strrep(filename,'.csv','');
+plotoutput = strcat("plots/",strrep(filename,'.csv',''),".pdf");
+
+lle_data = DataLoading(filename)
 
 ## Data parsing to get X and Y values from LLE data
 [x_values, y_values] = DataParser(lle_data);
@@ -17,14 +21,32 @@ x_minmax_matirx = MinMaxTransformer(x_values)
 # GrregX, mklegendrematrix, legendreclenshaw, legendrepol
 
 # creating legendre matrix for x transformed values
-x_legendre_matrix = mklegendrematrix(x_minmax_matirx, 2)
+x_legendre_matrix = mklegendrematrix(x_minmax_matirx, 3)
 # x - y values structure for GrregX
 xy_matrix.x = x_legendre_matrix;
 xy_matrix.y = y_values;
 
 # GrregX on xy matrix
 grregx_result = GrregX(xy_matrix)
+# coefficient from GrregX
+coefficients = grregx_result.coef;
+residuals = grregx_result.resid;
 
-## residual check
+## legendreclenshaw
+y_lc = legendreclenshaw(x_values, coefficients);
+y_lc_resid = y_values - y_lc;
+## residual for y_lc
+lc_reg_result = [y_values, y_lc, y_lc_resid]
+
+## legendrepol
+y_lpoly = legendrepol(x_values,5);
+y_lpoly_resid = y_values - y_lpoly;
+y_lpoly_result = [y_values, y_lpoly, y_lpoly_resid]
 
 ## plotting results 
+plot(x_values, y_values,'ok', x_values, y_lc, '*r')
+xlabel('mol fraction [x]')
+ylabel('Temperature [K]')
+legend(strcat(plottitle, '-Data'), strcat(plottitle, '-LReg'))
+title(plottitle)
+print(plotoutput)
