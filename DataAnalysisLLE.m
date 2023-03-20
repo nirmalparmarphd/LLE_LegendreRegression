a7d248c155f0520942a4ac32deb0fbcbf7b925b0
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-03-20 16:15:47 +0100

    function created for legendre reg and residuals

--- DataAnalysisLLE.m
+++ DataAnalysisLLE.m
@@ -1,52 +1,55 @@
 ## LLE Data Analysis with Legendre gnostic regression 
+## usage:   LReg_result = DataAnalysisLLE(filename,order)
+##          filename: ../data/filename.csv
+##          order: polynomial degree
 
+function LReg_result = DataAnalysisLLE(filename, order);
 ## pkg import
-clear
 pkg load io
+
+## creating directory for data and plots
+if not(isfolder('data'))
+    mkdir('data')
+end
+
+if not(isfolder('plots'))
+    mkdir('plots')
+end
 
 ## loading lle data
 filename = ('hexanol_109.csv');
 plottitle = strrep(filename,'.csv','');
 plotoutput = strcat("plots/",strrep(filename,'.csv',''),".pdf");
-
-lle_data = DataLoading(filename)
+lle_data = DataLoading(strcat("data/",filename))
 
 ## Data parsing to get X and Y values from LLE data
 [x_values, y_values] = DataParser(lle_data);
 
 ## MinMax transformaation of X vaues from LLE data
-x_minmax_matirx = MinMaxTransformer(x_values)
+x_minmax_matirx = MinMaxTransformer(x_values);
 
 ## regression of LLE data 
 # GrregX, mklegendrematrix, legendreclenshaw, legendrepol
 
 # creating legendre matrix for x transformed values
-x_legendre_matrix = mklegendrematrix(x_minmax_matirx, 3)
+x_legendre_matrix = mklegendrematrix(x_minmax_matirx, order);
 # x - y values structure for GrregX
 xy_matrix.x = x_legendre_matrix;
 xy_matrix.y = y_values;
 
 # GrregX on xy matrix
-grregx_result = GrregX(xy_matrix)
+LReg_result = GrregX(xy_matrix)
 # coefficient from GrregX
-coefficients = grregx_result.coef;
-residuals = grregx_result.resid;
-
-## legendreclenshaw
-y_lc = legendreclenshaw(x_values, coefficients);
-y_lc_resid = y_values - y_lc;
-## residual for y_lc
-lc_reg_result = [y_values, y_lc, y_lc_resid]
-
-## legendrepol
-y_lpoly = legendrepol(x_values,5);
-y_lpoly_resid = y_values - y_lpoly;
-y_lpoly_result = [y_values, y_lpoly, y_lpoly_resid]
+coefficients = LReg_result.coef;
+residuals = LReg_result.resid;
+fitY = LReg_result.fitY;
 
 ## plotting results 
-plot(x_values, y_values,'ok', x_values, y_lc, '*r')
+plot(x_values, y_values,'ok', x_values, fitY, '*r')
 xlabel('mol fraction [x]')
 ylabel('Temperature [K]')
 legend(strcat(plottitle, '-Data'), strcat(plottitle, '-LReg'))
 title(plottitle)
 print(plotoutput)
+
+endfunction
