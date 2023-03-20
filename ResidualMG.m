b370aa9678628198f07983359636974b928ba87f
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-03-20 17:02:24 +0100

    function updated

--- ResidualMG.m
+++ ResidualMG.m
@@ -1,11 +1,11 @@
 ## MG marginal analysis of residuals from LegReg
 ## usage:   intv = ResidualMG(para);
-##          para = to define parameters for dfchoice
+##          data = residuals data
 
 
 ## MG marginal analysis
-function intv = ResidualMG(para);
+function intv = ResidualMG(resid);
 para = dfchoice
-para.data = LegReg_result.resid;
+para.data = resid;
 [result, intv, LowerCluster, UpperCluster, ERR] = zw_marginal_analysis_egdf(para);
 endfunction
--- analysis.m
+++ analysis.m
@@ -4,4 +4,4 @@
 LegReg_result = LegReg('hexanol_109.csv', order=3);
 
 # MG marginal analysis of residual from LegReg
-intv = ResidualMG(para);
+intv = ResidualMG(LegReg_result.resid);
