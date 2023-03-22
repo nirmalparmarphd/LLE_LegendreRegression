0888ca47f6b74f19514ad045d07360bcb4b7dd59
nirmalparmar_ws <nirmalparmarphd@gmail.com>
2023-03-22 11:23:06 +0100

    function updated

--- KaBoOm.m
+++ KaBoOm.m
@@ -1,3 +1,20 @@
 # To run a series of LLE data analysis
 
-LegReg_result, intv, RegPlot, ResdPlot, Rdata = KaBoOm(filename='hexanol_109.csv', order =3, fn='X');
+filename = 'hexanol_109.csv';
+order = 3;
+fn = 'X';
+
+# LegReg: Legendre Regression
+LegReg_result = LegReg(filename, order, fn);
+
+# MG marginal analysis of residual from LegReg
+intv = ResidualMG(filename, LegReg_result.resid);
+
+# plotting fitY and experimental data
+plt_reg = RegPlot(filename, LegReg_result.fitY, fn);
+
+# plotting residuals
+plt_res = ResidPlot(filename, LegReg_result, intv, fn);
+
+# estimating the outliers from residuals
+rdata = OutliersRemover(filename, LegReg_result, intv, fn);
